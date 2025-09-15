991a74123a41433c67727b789c43b2a8f25ace9c
Palak Khatri <palakkhatri934@gmail.com>
2025-09-15 14:35:47 +0530

    file change databasejs

--- xeno-backend/config/database.js
+++ xeno-backend/config/database.js
@@ -1,15 +1,16 @@
+// xeno-backend/config/database.js
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-// Create a new Sequelize instance to connect to the database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Required for Neon
+    }
   }
-);
+});
 
 module.exports = sequelize;
